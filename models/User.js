8cad0e239e6e4222b6475c0403efe72d39bff3d1
Erika Cannon <cannone.rva@gmail.com>
2022-06-30 16:41:03 -0400

    file structure, set up User model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      //add email validation here
+      //enum: ['' + '@' + ''],
+    },
+    thoughts: [
+      {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+    friends: [
+      {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+},
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
