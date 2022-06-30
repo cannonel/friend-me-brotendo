e92c76bb4474f112ce887c0e04379289a15bae39
Erika Cannon <cannone.rva@gmail.com>
2022-06-30 17:51:50 -0400

    set up thought-route

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+
+// deconstruct and import all methods related to User
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  addReaction,
+  deleteReaction,
+  updateThought,
+  deleteThought,
+} = require("../../controllers/thought-controller");
+
+// GET routes
+router.route("/").get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+router.route("/:thoughtId/:reactionId").delete(deleteReaction);
+module.exports = router;
